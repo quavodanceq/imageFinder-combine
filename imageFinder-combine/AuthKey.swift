4a90bc474a11cc4ee693b9e1e37fe0cc170350bd
quavodanceq <kuat.oralbekov1011@mail.ru>
2023-06-04 22:19:01 +0600

    authKey

---
+++ imageFinder-combine/AuthKey.swift
@@ -0,0 +1,14 @@
+//
+//  AuthKey.swift
+//  imageFinder-combine
+//
+//  Created by Куат Оралбеков on 04.06.2023.
+//
+
+import Foundation
+
+class AuthKey {
+    
+    static let key = "BQwJow6uZ3aT9IuPcYtVyxQCpqytSXM2dHzZJz7ZAuUKdjN5469VR8EO"
+    
+}
